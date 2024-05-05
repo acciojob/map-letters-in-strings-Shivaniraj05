c5553ca0bf71cd9f91aaf1caee77cb00a36d5115
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-05-05 16:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function mapLetters(str) {
+	let ans = {};
+
+	for(let i=0; i<str.length; i++){
+		if(ans[str[i]]){
+			ans[str[i]].push(i);
+		}
+		else{
+			ans[str[i]] = [i];
+		}
+	}
+	
+	return ans;
+	
+}
+
+console.log(mapLetters(str));
